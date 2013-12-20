09b3567a1cfea42fe15002f9e139f09f54771014
anovi <mygrib@gmail.com>
2013-12-20 16:27:27 +0400

    Keyboard test suite

--- test/test.js
+++ test/test.js
@@ -84,16 +84,29 @@ QUnit.assert.selectedCount = function( count ) {
 QUnit.testStart( function (info) {
   var res  = $.extend( {}, initOptions ),
   advanced = {};
+
+  if (info.module === 'Keyboard')
+    $.extend( advanced, { multi: true, keyboardInput: true });
   
   switch ( info.name ) {
     case 'Blurable mousedown':
-      advanced = { focusBlur: true, selectionBlur: true };
+      $.extend( advanced, { focusBlur: true, selectionBlur: true });
       break;
     case 'Toggle mousedown':
-      advanced = { mouseMode: 'toggle' };
+      $.extend( advanced, { mouseMode: 'toggle' });
       break;
     case 'Hybrid mouse':
-      advanced = { event: 'hybrid' };
+      $.extend( advanced, { event: 'hybrid' });
+      break;
+    case 'Filter odd mousedown':
+    case 'Filter odd up/down':
+      $.extend( advanced, { filter: 'li:odd', selectionBlur: true });
+      break;
+    case 'Handled items mousedown':
+      $.extend( advanced, { handle: '.handle', selectionBlur: true });
+      break;
+    case 'Loop':
+      $.extend( advanced, { loop: true });
       break;
   }
 
@@ -141,8 +154,9 @@ test( 'Select mousedown', 4, function() {
 
 test( 'Multi-select mousedown', 6, function() {
   var
-  elem          = getBox().find('li:eq(3)'),
-  secElem       = getBox().find('li:eq(5)'),
+  box           = getBox(),
+  elem          = box.find('li:eq(3)'),
+  secElem       = box.find('li:eq(5)'),
   e             = $.Event( "mousedown" );
   e.metaKey     = true;
   
@@ -164,10 +178,10 @@ test( 'Multi-select mousedown', 6, function() {
 
 test( 'Range-select mousedown', 6, function() {
   var
-  selected,
-  elem       = getBox().find('li:eq(3)'),
-  midElem    = getBox().find('li:eq(4)'),
-  secElem    = getBox().find('li:eq(5)'),
+  box        = getBox(),
+  elem       = box.find('li:eq(3)'),
+  midElem    = box.find('li:eq(4)'),
+  secElem    = box.find('li:eq(5)'),
   e          = $.Event( "mousedown" );
   e.shiftKey = true;
   
@@ -197,10 +211,10 @@ test( 'Blurable mousedown', 3, function() {
 
 test( 'Toggle mousedown', 4, function() {
   var
-  selected,
-  elem       = getBox().find('li:eq(3)'),
-  midElem    = getBox().find('li:eq(4)'),
-  secElem    = getBox().find('li:eq(5)'),
+  box        = getBox(),
+  elem       = box.find('li:eq(3)'),
+  midElem    = box.find('li:eq(4)'),
+  secElem    = box.find('li:eq(5)'),
   e          = $.Event( "mousedown" );
   e.shiftKey = true;
   
@@ -216,10 +230,10 @@ test( 'Toggle mousedown', 4, function() {
 
 asyncTest( 'Hybrid mouse', 3, function() {
   var
-  selected,
-  elem       = getBox().find('li:eq(3)'),
-  midElem    = getBox().find('li:eq(4)'),
-  secElem    = getBox().find('li:eq(5)'),
+  box        = getBox(),
+  elem       = box.find('li:eq(3)'),
+  midElem    = box.find('li:eq(4)'),
+  secElem    = box.find('li:eq(5)'),
   e          = $.Event( "mousedown" );
   e.shiftKey = true;
   
@@ -248,12 +262,142 @@ asyncTest( 'Hybrid mouse', 3, function() {
   //   console.log('is selected?');
   //   ok( midElem.hasClass('selected'), "Selected" );
   //   ok( midElem.hasClass('focused'), "Focused" );
-  //   selected = getBox().find('.selected');
+  //   selected = box.find('.selected');
   //   equal( selected.length, 1, "1 selected" );
   //   start();
   // });
 });
 
+test( 'Filter odd mousedown', 4, function() {
+  var
+  box        = getBox(),
+  elem       = box.find('li:eq(3)'),
+  midElem    = box.find('li:eq(4)'),
+  secElem    = box.find('li:eq(5)'),
+  e          = $.Event( "mousedown" );
+  e.shiftKey = true;
+  
+  elem.trigger('mousedown');
+  secElem.trigger( e );
+  assert.selected( elem );
+  assert.selectedFocus( secElem );
+  assert.notSelected( midElem );
+  
+  midElem.trigger('mousedown');
+  assert.selectedCount(0);
+});
+
+test( 'Handled items mousedown', 2, function() {
+  var elem = getBox().find('li:eq(3)');
+  
+  elem.trigger('mousedown');
+  assert.selectedCount(0);
+
+  elem.find('.handle').trigger('mousedown');
+  assert.selected( elem );
+});
+
+
+/*
+*
+* Keyboard suite
+*
+*/
+module("Keyboard");
+
+test( 'Up/down and shift', 6, function() {
+  var
+  box        = getBox(),
+  elem       = box.find('li:eq(3)'),
+  midElem    = box.find('li:eq(4)'),
+  secElem    = box.find('li:eq(5)');
+  
+  Syn.type( '[down]', box );
+  Syn.type( '[down]', box );
+  Syn.type( '[down]', box );
+  Syn.type( '[down]', box );
+  assert.selected( elem );
+  assert.selectedCount(1);
+
+  Syn.type( '[shift]', box );
+  Syn.type( '[down]', box );
+  Syn.type( '[down]', box );
+  Syn.type( '[shift-up]', box );
+  assert.selected( elem );
+  assert.selected( midElem );
+  assert.selectedFocus( secElem );
+  assert.selectedCount(3);
+});
+
+test( 'PageUp/PageDown and shift', 4, function() {
+  var
+  box   = getBox(),
+  first = box.find('li').first(),
+  last  = box.find('li').last();
+  
+  Syn.type( '[down]', box );
+  Syn.type( '[shift][end]', box );
+  Syn.type( '[shift-up]', box );
+  assert.selectedFocus( last );
+  assert.selectedCount(20);
+
+  Syn.type( '[shift][home]', box );
+  Syn.type( '[shift-up]', box );
+  assert.focused( first );
+  assert.selectedCount(0);
+});
+
+test( 'Ctrl+A', 2, function() {
+  var
+  box   = getBox(),
+  first = box.find('li').first(),
+  last  = box.find('li').last();
+  
+  Syn.type( '[ctrl]a', box );
+  Syn.type( '[ctrl-up]', box );
+  assert.selectedFocus( first );
+  assert.selectedCount(20);
+});
+
+test( 'Loop', 5, function() {
+  var
+  box   = getBox(),
+  first = box.find('li').first(),
+  last  = box.find('li').last();
+  
+  Syn.type( '[down]', box );
+  Syn.type( '[shift][up]', box );
+  Syn.type( '[shift-up]', box );
+  assert.selectedFocus( last );
+  assert.selected( first );
+  assert.selectedCount(2);
+
+  Syn.type( '[down]', box );
+  assert.selectedFocus( first );
+  assert.selectedCount(1);
+});
+
+test( 'Filter odd up/down', 5, function() {
+  var
+  box   = getBox(),
+  first = box.find('li:eq(1)'),
+  sec   = box.find('li:eq(3)'),
+  last  = box.find('li:eq(5)');
+  
+  Syn.type( '[down]', box );
+  assert.selectedFocus( first );
+  
+  Syn.type( '[shift][down]', box );
+  Syn.type( '[down]', box );
+  Syn.type( '[shift-up]', box );
+  assert.selectedFocus( last );
+  assert.selected( sec );
+  assert.selectedCount( 3 );
+
+  Syn.type( '[ctrl]a', box );
+  Syn.type( '[ctrl-up]', box );
+  assert.selectedCount( 10 );
+});
 
 
 /*
@@ -298,5 +442,4 @@ Public methods:
   cancel
   refresh
 */
-
 
