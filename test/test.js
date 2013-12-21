d19711e293e33612c03d9bb6f81420dd3956e0ab
anovi <mygrib@gmail.com>
2013-12-21 19:21:02 +0400

    Suite for API

--- test/test.js
+++ test/test.js
@@ -47,32 +47,38 @@ getBox = function () {
 QUnit.assert.selected = function( elem ) {
   var actual = elem.hasClass('selected');
   QUnit.push(actual, actual, true, 'Selected');
+  return QUnit.assert;
 };
 
 QUnit.assert.focused = function( elem ) {
   var actual = elem.hasClass('focused');
   QUnit.push(actual, actual, true, 'Focused');
+  return QUnit.assert;
 };
 
 QUnit.assert.notSelected = function( elem ) {
   var actual = !elem.hasClass('selected');
   QUnit.push(actual, actual, true, 'Not selected!');
+  return QUnit.assert;
 };
 
 QUnit.assert.notFocused = function( elem ) {
   var actual = !elem.hasClass('focused');
   QUnit.push(actual, actual, true, 'Not focused!');
+  return QUnit.assert;
 };
 
 QUnit.assert.selectedFocus = function( elem ) {
   var actual = elem.hasClass('focused') && elem.hasClass('selected');
   QUnit.push(actual, actual, true, 'Focused and selected!');
+  return QUnit.assert;
 };
 
 QUnit.assert.selectedCount = function( count ) {
   var selected = getBox().find('.selected');
   var actual = selected.length === count;
   QUnit.push(actual, actual, true, 'Is ' + count + ' selected!');
+  return QUnit.assert;
 };
 
 
@@ -107,6 +113,9 @@ QUnit.testStart( function (info) {
       break;
     case 'Loop':
       $.extend( advanced, { loop: true });
+      break;
+    case 'refresh':
+      $.extend( advanced, { selectionBlur: true, focusBlur: true });
       break;
   }
 
@@ -147,9 +156,10 @@ test( 'Select mousedown', 4, function() {
   assert.selectedFocus( elem );
   
   secElem.trigger('mousedown');
-  assert.selectedFocus( secElem );
-  assert.notSelected( elem );
-  assert.notFocused( elem );
+  assert
+    .selectedFocus( secElem )
+    .notSelected( elem )
+    .notFocused( elem );
 });
 
 test( 'Multi-select mousedown', 6, function() {
@@ -165,15 +175,17 @@ test( 'Multi-select mousedown', 6, function() {
   ok( elem.hasClass('focused'), "Focused" );
 
   secElem.trigger( e );
-  assert.selected( elem );
-  assert.selectedFocus( secElem );
+  assert
+    .selected( elem )
+    .selectedFocus( secElem );
 
   elem.trigger('mousedown');
   e = $.Event( "mousedown" );
   e.metaKey = true;
   elem.trigger( e );
-  assert.notSelected( elem );
-  assert.focused( elem );
+  assert
+    .notSelected( elem )
+    .focused( elem );
 });
 
 test( 'Range-select mousedown', 6, function() {
@@ -189,13 +201,15 @@ test( 'Range-select mousedown', 6, function() {
   assert.selectedFocus( elem );
 
   secElem.trigger( e );
-  assert.selected( elem );
-  assert.selected( midElem );
-  assert.selectedFocus( secElem );
+  assert
+    .selected( elem )
+    .selected( midElem )
+    .selectedFocus( secElem );
 
   midElem.trigger('mousedown');
-  assert.selectedFocus( midElem );
-  assert.selectedCount(1);
+  assert
+    .selectedFocus( midElem )
+    .selectedCount(1);
 });
 
 test( 'Blurable mousedown', 3, function() {
@@ -220,12 +234,14 @@ test( 'Toggle mousedown', 4, function() {
   
   elem.trigger('mousedown');
   secElem.trigger('mousedown');
-  assert.selected( elem );
-  assert.selectedFocus( secElem );
+  assert
+    .selected( elem )
+    .selectedFocus( secElem );
   
   elem.trigger('mousedown');
-  assert.notSelected( elem );
-  assert.focused( elem );
+  assert
+    .notSelected( elem )
+    .focused( elem );
 });
 
 asyncTest( 'Hybrid mouse', 3, function() {
@@ -243,8 +259,9 @@ asyncTest( 'Hybrid mouse', 3, function() {
   secElem.trigger( e );
   
   Syn.click( {}, midElem, function () {
-    assert.selectedFocus( midElem );
-    assert.selectedCount(1);
+    assert
+      .selectedFocus( midElem )
+      .selectedCount(1);
     start();
   });
   
@@ -279,9 +296,10 @@ test( 'Filter odd mousedown', 4, function() {
   
   elem.trigger('mousedown');
   secElem.trigger( e );
-  assert.selected( elem );
-  assert.selectedFocus( secElem );
-  assert.notSelected( midElem );
+  assert
+    .selected( elem )
+    .selectedFocus( secElem )
+    .notSelected( midElem );
   
   midElem.trigger('mousedown');
   assert.selectedCount(0);
@@ -316,17 +334,19 @@ test( 'Up/down and shift', 6, function() {
   Syn.type( '[down]', box );
   Syn.type( '[down]', box );
   Syn.type( '[down]', box );
-  assert.selected( elem );
-  assert.selectedCount(1);
+  assert
+    .selected( elem )
+    .selectedCount(1);
 
   Syn.type( '[shift]', box );
   Syn.type( '[down]', box );
   Syn.type( '[down]', box );
   Syn.type( '[shift-up]', box );
-  assert.selected( elem );
-  assert.selected( midElem );
-  assert.selectedFocus( secElem );
-  assert.selectedCount(3);
+  assert
+    .selected( elem )
+    .selected( midElem )
+    .selectedFocus( secElem )
+    .selectedCount(3);
 });
 
 test( 'PageUp/PageDown and shift', 4, function() {
@@ -338,13 +358,15 @@ test( 'PageUp/PageDown and shift', 4, function() {
   Syn.type( '[down]', box );
   Syn.type( '[shift][end]', box );
   Syn.type( '[shift-up]', box );
-  assert.selectedFocus( last );
-  assert.selectedCount(20);
+  assert
+    .selectedFocus( last )
+    .selectedCount(20);
 
   Syn.type( '[shift][home]', box );
   Syn.type( '[shift-up]', box );
-  assert.focused( first );
-  assert.selectedCount(0);
+  assert
+    .focused( first )
+    .selectedCount(0);
 });
 
 test( 'Ctrl+A', 2, function() {
@@ -355,8 +377,9 @@ test( 'Ctrl+A', 2, function() {
   
   Syn.type( '[ctrl]a', box );
   Syn.type( '[ctrl-up]', box );
-  assert.selectedFocus( first );
-  assert.selectedCount(20);
+  assert
+    .selectedFocus( first )
+    .selectedCount(20);
 });
 
 test( 'Loop', 5, function() {
@@ -368,13 +391,15 @@ test( 'Loop', 5, function() {
   Syn.type( '[down]', box );
   Syn.type( '[shift][up]', box );
   Syn.type( '[shift-up]', box );
-  assert.selectedFocus( last );
-  assert.selected( first );
-  assert.selectedCount(2);
+  assert
+    .selectedFocus( last )
+    .selected( first )
+    .selectedCount(2);
 
   Syn.type( '[down]', box );
-  assert.selectedFocus( first );
-  assert.selectedCount(1);
+  assert
+    .selectedFocus( first )
+    .selectedCount(1);
 });
 
 test( 'Filter odd up/down', 5, function() {
@@ -390,14 +415,273 @@ test( 'Filter odd up/down', 5, function() {
   Syn.type( '[shift][down]', box );
   Syn.type( '[down]', box );
   Syn.type( '[shift-up]', box );
-  assert.selectedFocus( last );
-  assert.selected( sec );
-  assert.selectedCount( 3 );
+  assert
+    .selectedFocus( last )
+    .selected( sec )
+    .selectedCount( 3 );
 
   Syn.type( '[ctrl]a', box );
   Syn.type( '[ctrl-up]', box );
   assert.selectedCount( 10 );
 });
+
+
+/*
+*
+* API suite
+*
+*/
+module("API");
+
+test( 'Disable/enable', 4, function() {
+  var
+  box     = getBox(),
+  elem    = box.find('li:eq(3)'),
+  secElem = box.find('li:eq(5)'),
+  
+  isEnabled = box
+    .multiSelectable('li:eq(3)')
+    .multiSelectable('disable')
+    .multiSelectable('isEnabled');
+  ok( !isEnabled, 'Is diabled!' );
+  
+  secElem.trigger('mousedown');
+  assert
+    .selectedFocus( elem )
+    .selectedCount( 1 );
+  
+  isEnabled = box.multiSelectable('enable').multiSelectable('isEnabled');
+  ok( isEnabled, 'Is enabled!' );
+});
+
+test( 'blur', 6, function() {
+  var
+  box     = getBox(),
+  elem    = box.find('li:eq(3)'),
+  secElem = box.find('li:eq(5)');
+  
+  box.multiSelectable('li:eq(3)');
+  assert
+    .selectedFocus( elem )
+    .selectedCount( 1 );
+  box
+    .multiSelectable( 'option', 'selectionBlur', true )
+    .multiSelectable( 'blur' );
+  assert
+    .focused( elem )
+    .selectedCount( 0 );
+  box
+    .multiSelectable('li:eq(3)')
+    .multiSelectable( 'option', 'focusBlur', true )
+    .multiSelectable( 'blur' );
+  assert
+    .notFocused( elem )
+    .selectedCount( 0 );
+});
+
+test( 'getSelected', 4, function() {
+  var
+  box     = getBox(),
+  elem    = box.find('li:eq(3)'),
+  secElem = box.find('li:eq(5)'),
+  selected;
+  
+  Syn.click( {}, elem );
+  Syn.type('[shift]', box);
+  Syn.click( {}, secElem );
+  Syn.type('[shift-up]', box);
+
+  assert
+    .selectedFocus( secElem )
+    .selectedCount( 3 );
+  
+  selected = box
+    .multiSelectable('option', 'selectionBlur', true)
+    .multiSelectable('getSelected');
+  equal( selected.length, 3, "3 selected" );
+
+  $('body').trigger('mousedown');
+
+  selected = box.multiSelectable('getSelected');
+  equal( selected.length, 0, "0 selected" );
+});
+
+test( 'getSelectedId', 5, function() {
+  var
+  box     = getBox(),
+  elem    = box.find('li:eq(3)'),
+  secElem = box.find('li:eq(5)'),
+  selected;
+  
+  Syn.click( {}, elem );
+  Syn.type('[shift]', box);
+  Syn.click( {}, secElem );
+  Syn.type('[shift-up]', box);
+
+  assert
+    .selectedFocus( secElem )
+    .selectedCount( 3 );
+  
+  selected = box
+    .multiSelectable('option', 'selectionBlur', true)
+    .multiSelectable('getSelectedId');
+  ok( $.isArray(selected), 'Is array' );
+  equal( selected.length, 3, "Is 3 selected" );
+  equal( selected[0], elem.attr('id'), "Id's match" );
+});
+
+test( 'getSelectedId', 2, function() {
+  var
+  box     = getBox(),
+  elem    = box.find('li:eq(3)'),
+  selected;
+  
+  Syn.click( {}, elem );
+  selected = box
+    .multiSelectable( 'option', 'focusBlur', true )
+    .multiSelectable( 'getFocused' );
+  ok( $(selected).is( elem ) , 'Items match' );
+
+  $('body').trigger('mousedown');
+  selected = box.multiSelectable( 'getFocused' );
+  ok( selected === null, 'No focus' );
+});
+
+test( 'select', 2, function() {
+  var
+  box     = getBox(),
+  elem    = box.find('li:eq(3)');
+  
+  box.multiSelectable( elem );
+  assert.selected( elem );
+  
+  box.multiSelectable( 'li:odd' );
+  assert.selectedCount( 10 );
+});
+
+test( 'refresh', 3, function() {
+  var
+  box     = getBox(),
+  elem    = box.find('li:eq(0)'),
+  check   = true;
+  
+  box
+    .multiSelectable( elem )
+    .multiSelectable( 'option', { unSelectAll: function(e, ui) {check = false;} });
+  
+  assert.selectedCount( 1 );
+
+  elem.remove();
+  box
+    .multiSelectable('refresh')
+    .multiSelectable('blur');
+
+  assert.selectedCount( 0 );
+  ok( check, 'There was no unSelectAll callback!' );
+});
+
+test( 'cancel', 2, function() {
+  var
+  box     = getBox(),
+  elem    = box.find('li:eq(0)');
+  
+  box
+    .multiSelectable( 'li:odd' )
+    .multiSelectable( 'option', {
+      stop: function( e, ui ) {
+        this.multiSelectable( 'cancel' );
+      }
+    })
+    .multiSelectable( elem );
+
+  assert.selectedCount( 10 );
+  assert.notSelected( elem );
+});
+
+test( 'option', 6, function() {
+  var
+  box  = getBox(),
+  elem = box.find('li:eq(1)'),
+  sec  = box.find('li:eq(3)'),
+  res  = [],
+  options;
+  
+  box
+    .multiSelectable( 'option', {
+      beforeSelect: function(e, ui) { res.push( 'beforeSelect' ); },
+      focusLost:    function(e, ui) { res.push( 'focusLost' );    },
+      select:       function(e, ui) { res.push( 'select' );       },
+      unSelect:     function(e, ui) { res.push( 'unSelect' );     },
+      unSelectAll:  function(e, ui) { res.push( 'unSelectAll' );  },
+      stop:         function(e, ui) { res.push( 'stop' );         },
+      destroy:      function(e, ui) { res.push( 'destroy' );      },
+      
+      filter:        'li:odd',
+      mouseMode:     'toggle',
+      event:         'hybrid',
+      handle:        '.handle',
+
+      multi:         false,
+      scrolledElem:  false,
+      preventInputs: false,
+      
+      focusBlur:     true,
+      selectionBlur: true,
+      keyboardInput: true,
+      loop:          true
+    });
+
+  options = box.multiSelectable( 'option' );
+  ok((
+    options.filter        === 'li:odd' &&
+    options.mouseMode     === 'toggle' &&
+    options.event         === 'hybrid' &&
+    options.handle        === '.handle' &&
+    options.multi         === false &&
+    options.scrolledElem  === false &&
+    options.preventInputs === false &&
+    options.focusBlur     === true &&
+    options.selectionBlur === true &&
+    options.keyboardInput === true &&
+    options.loop          === true ),
+  'Options assigned!');
+
+  Syn.click( {}, elem.find('.handle') );
+  ok((
+    res[0] === 'beforeSelect' &&
+    res[1] === 'select' &&
+    res[2] === 'stop'
+  ));
+
+  res = [];
+  Syn.click( {}, $('body') );
+  ok((
+    res[0] === 'beforeSelect' &&
+    res[1] === 'focusLost' &&
+    res[2] === 'unSelect' &&
+    res[3] === 'unSelectAll' &&
+    res[4] === 'stop'
+  ));
+
+  res = [];
+  Syn.click( {}, $('body') );
+  ok((res[0] === 'beforeSelect' && res[1] === 'stop'));
+
+  Syn.click( {}, elem.find('.handle') );
+  res = [];
+  Syn.click( {}, sec.find('.handle') );
+  ok((
+    res[0] === 'beforeSelect' &&
+    res[1] === 'unSelect' &&
+    res[2] === 'select' &&
+    res[3] === 'stop'
+  ));
+
+  res = [];
+  box.multiSelectable( 'destroy' );
+  ok( res[0] === 'destroy' );
+});
+
 
 
 /*
@@ -430,16 +714,17 @@ Options:
 
 Public methods:
   isEnabled
-  option
   destroy
-  select
+  enable
+  disable
   blur
   getSelected
   getSelectedId
   getFocused
-  enable
-  disable
-  cancel
+  select
   refresh
+  cancel
+  option
+  
 */
 
