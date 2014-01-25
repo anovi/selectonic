ecbb0df32e48c76ae67f8b956e24a61c26d266b2
anovi <mygrib@gmail.com>
2014-01-26 00:50:44 +0400

    Callbacks edded.

--- src/app/scripts/scrollspy.js
+++ src/app/scripts/scrollspy.js
@@ -3,8 +3,15 @@
 
   var defaults = {
     box:    { type: 'object' },
-    offset: { default:0,    type:'number' },
-    clone:  { default:null, type:'function', isNullable:true }
+    offset: { default:0, type:'number' },
+    // Callbacks
+    clone:        { default:null, type:'function', isNullable:true },
+    cloneRemove:  { default:null, type:'function', isNullable:true },
+    move:         { default:null, type:'function', isNullable:true },
+    stop:         { default:null, type:'function', isNullable:true },
+    over:         { default:null, type:'function', isNullable:true },
+    out:          { default:null, type:'function', isNullable:true },
+    visible:      { default:null, type:'function', isNullable:true }
   },
   $window = $( window );
 
@@ -16,6 +23,7 @@
       position: this.$el.css('position'),
       top:      this.$el.css('top'),
     };
+    this._called = {};
     this.$el.data( 'scrollSpy', this );
     this._bindEvents();
   };
@@ -37,15 +45,26 @@
       $box          = $( box ),
       isWindow      = box === window,
       boxViewHeight = isWindow ? $box.outerHeight() : box.clientHeight,
-      // boxScrollTop  = $box.scrollTop(),
+      boxScrollTop  = $box.scrollTop(),
       boxWindowY    = isWindow ? 0 : $box.offset().top,
 
-      // windowViewHeight = $window.outerHeight(),
+      windowViewHeight = $window.outerHeight(),
       windowScrollTop = $window.scrollTop(),
 
       item       = this.$el,
-      itemHeight = item.outerHeight();
-      // itemInBoxY = isWindow ? item.offset().top : ( item.offset().top - boxWindowY + boxScrollTop );
+      itemHeight = item.outerHeight(),
+      itemInBoxY = isWindow ? item.offset().top : ( item.offset().top - boxWindowY + boxScrollTop );
+
+    if ( windowScrollTop + windowViewHeight > boxWindowY && boxWindowY + boxViewHeight > windowScrollTop) {
+      this._callEvent('over');
+      this._callEvent('visible', {
+        window: { height: windowViewHeight, scrollTop: windowScrollTop },
+        item:   { height: itemHeight,       topInBox:  itemInBoxY      },
+        box:    { height: boxViewHeight,    top:       boxWindowY      }
+      });
+    } else {
+      this._callEvent('out');
+    }
 
     if ( boxWindowY > windowScrollTop + offset) {
       item.css({
@@ -61,6 +80,7 @@
         position:'absolute',
         top: boxViewHeight + boxWindowY - itemHeight
       });
+      this._callEvent('stop');
     
     } else {
       this._setClone();
@@ -68,18 +88,19 @@
         position: 'fixed',
         top: 0 + this.options.get('offset')
       });
+      this._callEvent('move');
     }
   };
 
 
   Plugin.prototype._setClone = function() {
     if ( this._clone ) { return; }
-    this.$el.addClass('scrollspy-cloned');
     this._clone = this.$el
       .clone()
       .css({ opacity: 0 })
       .attr('id', null);
 
+    this._clone.addClass('scrollSpy-clone');
     this._callEvent('clone');
     this._clone.insertBefore( this.$el );
   };
@@ -87,21 +108,49 @@
 
   Plugin.prototype._cutClone = function() {
     if (this._clone) {
+      this._callEvent('cloneRemove');
       this._clone.remove();
       delete this._clone;
-      this.$el.removeClass('scrollspy-cloned');
     }
   };
 
 
-  Plugin.prototype._callEvent = function( name ) {
-    var cb = this.options.get( name ), args = [];
-    if ( !cb || !$.isFunction(cb) ) { return; }
+  Plugin.prototype._callEvent = function( name, params ) {
+    var cb = this.options.get( name ), args = [],
+    noCallback = !cb || !$.isFunction(cb);
 
-    if ( name === 'clone' ) {
+    switch (name) {
+    case 'move':
+      this._called.stop = false;
+      break;
+
+    case 'visible':
+      if ( noCallback ) { return; }
+      args.push( params );
+      break;
+
+    case 'clone':
+      if ( noCallback ) { return; }
       args.push( this._clone );
       args.push( this.$el );
+      break;
+      
+    case 'out':
+    case 'over':
+      if ( this._called[name] ) {
+        return;
+      } else {
+        this._called[name] = true;
+        this._called[ ( name === 'out' ? 'over' : 'out' ) ] = false;
+      }
+      break;
+
+    case 'stop':
+      if ( this._called.stop ) { return; }
+      this._called.stop = true;
+      break;
     }
+    if ( noCallback ) { return; }
     cb.apply( this, args );
   };
 
